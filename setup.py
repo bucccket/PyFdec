2f4e30b6bd80f2ab0412aff533c5c6533688ab2a
Bucccket <37452311+bucccket@users.noreply.github.com>
2024-08-07 23:47:51 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md') as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    name = 'pyfdec',
+    version = '1.0.0',
+    url = 'https://github.com/bucccket/PyFdec',
+    author = 'bucccket',
+    author_email = '',
+    license = 'GPL-3.0',
+    description = 'A libary for reading and writing to SWF files',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    install_requires = [
+        'bitio',
+    ],
+    py_modules = [
+        'pyfdec',
+    ],
+)
---
+++ tests/test_basictypes.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestSomething(TestCase):
+    def test_something(self):
+        self.assertEqual(1, 1)
