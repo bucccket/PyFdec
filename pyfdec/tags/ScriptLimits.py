50e7917bf8e6d1a9ebcc0fb05947c2c6c0adf1cb
moffel1020 <moffel1020@gmail.com>
2024-08-19 17:28:25 +0200

    add Metadata, ScriptLimits and SymbolClass tags

---
+++ pyfdec/tags/ScriptLimits.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from typing import ClassVar
+from pyfdec.extended_buffer import ExtendedBuffer
+from pyfdec.tags.Tag import Tag
+
+
+@dataclass
+class ScriptLimits(Tag):
+    tag_type: ClassVar[Tag.TagTypes] = Tag.TagTypes.ScriptLimits
+
+    max_recursion_depth: int
+    script_timeout_seconds: int
+
+    @classmethod
+    def from_buffer(cls, buffer: ExtendedBuffer):
+        max_recursion_depth = buffer.read_ui16()
+        script_timeout_seconds = buffer.read_ui16()
+        return cls(max_recursion_depth, script_timeout_seconds)
+
+
+Tag.register(ScriptLimits)
