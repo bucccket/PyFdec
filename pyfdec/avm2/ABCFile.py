22e85656630cb3147f86f242c29d79c479d3367f
moffel1020 <moffel1020@gmail.com>
2024-08-20 14:38:55 +0200

    add methodinfo

--- pyfdec/avm2/ABCFile.py
+++ pyfdec/avm2/ABCFile.py
@@ -1,24 +1,40 @@
 from collections import namedtuple
 from dataclasses import dataclass
 from pyfdec.extended_buffer import ExtendedBuffer
-from pyfdec.avm2.Multinames import * 
+from pyfdec.avm2.Multinames import BaseMultiname, QName, RTQName, RTQNameL, Multiname, MultinameL, TypeName
+from enum import Enum, IntFlag
 
 
 @dataclass
 class ABCFile:
-    minor_version: int
-    major_version: int
 
     @dataclass
     class CPoolInfo:
+        @dataclass
+        class NamespaceInfo:
+            class NamespaceKind(Enum):
+                Namespace = 0x08
+                PackageNamespace = 0x16
+                PackageInternalNs = 0x17
+                ProtectedNamespace = 0x18
+                ExplicitNamespace = 0x19
+                StaticProtectedNs = 0x1A
+                PrivateNs = 0x05
+
+            kind: NamespaceKind
+            name: int
+
+            @classmethod
+            def from_buffer(cls, buffer: ExtendedBuffer):
+                kind = cls.NamespaceKind(buffer.read_ui8())
+                name = buffer.read_encoded_u30()
+                return cls(kind, name)
+
         ints: list[int]
         uints: list[int]
         doubles: list[float]
         strings: list[str]
-
-        NamespaceInfo = namedtuple("NamespaceInfo", ["kind", "name"])
         namespaces: list[NamespaceInfo]
-
         namespace_sets: list[list[int]]
         multinames: list[BaseMultiname]
 
@@ -34,7 +50,7 @@ class ABCFile:
             strings = [cls.read_abc_string(buffer) for _ in range(string_count - 1)]
             namespace_count = buffer.read_encoded_u30()
             namespaces = [
-                cls.NamespaceInfo(kind=buffer.read(1), name=buffer.read_encoded_u30()) 
+                cls.NamespaceInfo.from_buffer(buffer)
                 for _ in range(namespace_count - 1)
             ]
             namespace_set_count = buffer.read_encoded_u30()
@@ -81,17 +97,106 @@ class ABCFile:
                 case _:
                     raise NotImplementedError(f"Unimplemented multiname type: {kind}")
 
+    @dataclass
+    class MethodInfo:
+        class MethodFlags(IntFlag):
+            NeedArguments = 0x01
+            NeedActivation = 0x02
+            NeedRest = 0x04
+            HasOptional = 0x08
+            SetDxns = 0x40
+            HasParamNames = 0x80
 
+        @dataclass
+        class OptionInfo:
+            @dataclass
+            class OptionDetail:
+                class ConstandKind(Enum):
+                    Int = 0x03
+                    UInt = 0x04
+                    Double = 0x06
+                    Utf8 = 0x01
+                    Bool_True = 0x0B
+                    Bool_False = 0x0A
+                    Null = 0x0C
+                    Undefined = 0x00
+                    Namespace = 0x08
+                    PackageNamespace = 0x16
+                    PackageInternalNs = 0x17
+                    ProtectedNamespace = 0x18
+                    ExplicitNamespace = 0x19
+                    StaticProtectedNs = 0x1A
+                    PrivateNs = 0x05
+
+                value: int
+                kind: ConstandKind
+
+                @classmethod
+                def from_buffer(cls, buffer: ExtendedBuffer):
+                    value = buffer.read_encoded_u30()
+                    kind = cls.ConstandKind(buffer.read_ui8())
+                    return cls(value, kind)
+
+
+            details: list[OptionDetail]
+
+            @classmethod
+            def from_buffer(cls, buffer: ExtendedBuffer):
+                detail_count = buffer.read_encoded_u30()
+                details = [cls.OptionDetail.from_buffer(buffer) for _ in range(detail_count)]
+                return cls(details)
+
+
+        return_type: int
+        param_types: list[int]
+        name: int
+        flags: list[MethodFlags]
+        option_info: list[OptionInfo] | None
+        param_names: list[int] | None
+
+        @classmethod
+        def from_buffer(cls, buffer: ExtendedBuffer):
+            param_count = buffer.read_encoded_u30()
+            return_type = buffer.read_encoded_u30()
+            param_types = [buffer.read_encoded_u30() for _ in range(param_count)]
+
+            name = buffer.read_encoded_u30()
+            flags = cls.MethodFlags(buffer.read_ui8())
+
+            options = None
+            if flags & cls.MethodFlags.HasOptional:
+                options = cls.OptionInfo.from_buffer(buffer)
+            
+            param_names = None
+            if flags & cls.MethodFlags.HasParamNames:
+                param_names = [buffer.read_encoded_u30() for _ in range(param_count)]
+            
+            return cls(
+                return_type,
+                param_types,
+                name,
+                flags,
+                options,
+                param_names
+            )
+
+
+    minor_version: int
+    major_version: int
     cpool: CPoolInfo
+    method_info: list[MethodInfo]
     
     @classmethod
     def from_buffer(cls, buffer: ExtendedBuffer):
         minor_version = buffer.read_ui16()
         major_version = buffer.read_ui16()
         cpool = cls.CPoolInfo.from_buffer(buffer)
+        method_count = buffer.read_encoded_u30()
+        method_info = [cls.MethodInfo.from_buffer(buffer) for _ in range(method_count)]
 
         return cls(
             minor_version,
             major_version,
-            cpool
+            cpool,
+            method_info
         )
