bdcc16344142f12aa735004bef98a68b597b32dd
bucccket <nicolas.lisgaras@gmail.com>
2024-09-13 01:26:33 +0200

    fixed test (temporary fix)

--- tests/test_swf.py
+++ tests/test_swf.py
@@ -14,7 +14,7 @@ class TestSwfHeader(TestCase):
         with open("tests/swf/Gfx_Ahsoka_Sword.swf", "rb") as file:
             buffer = ExtendedBuffer(file.read())
             swf_header, buffer = SwfHeader.from_buffer(buffer)
-            self.assertEqual(swf_header.compression, SwfHeader.CompressionLevel.NONE)
+            self.assertEqual(swf_header.compression, SwfHeader.CompressionLevel.ZLIB)
             self.assertEqual(swf_header.version, 15)
             self.assertEqual(swf_header.fileLength, 17980)
             self.assertEqual(swf_header.frameCount, 1)
@@ -56,15 +56,16 @@ class TestSwfFile(TestCase):
             abc_tag: DoABC = [
                 tag for tag in swf.tags if tag.tag_type == tag.TagTypes.DoABC
             ][0]
-            self.assertEqual(len(abc_tag.ABCData.cpool.ints), 864)
-            self.assertEqual(len(abc_tag.ABCData.cpool.uints), 214)
-            self.assertEqual(len(abc_tag.ABCData.cpool.doubles), 644)
-            self.assertEqual(len(abc_tag.ABCData.cpool.strings), 33138)
-            self.assertEqual(len(abc_tag.ABCData.cpool.namespaces), 35)
+            # FIXME: due to new updates, this test will break if the wrong version of BhAir is used
+            self.assertEqual(len(abc_tag.ABCData.cpool.ints), 865)
+            self.assertEqual(len(abc_tag.ABCData.cpool.uints), 219)
+            self.assertEqual(len(abc_tag.ABCData.cpool.doubles), 652)
+            self.assertEqual(len(abc_tag.ABCData.cpool.strings), 33623)
+            self.assertEqual(len(abc_tag.ABCData.cpool.namespaces), 36)
             self.assertEqual(len(abc_tag.ABCData.cpool.namespace_sets), 3)
-            self.assertEqual(len(abc_tag.ABCData.cpool.multinames), 23870)
-            self.assertEqual(len(abc_tag.ABCData.methods), 12095)
+            self.assertEqual(len(abc_tag.ABCData.cpool.multinames), 24257)
+            self.assertEqual(len(abc_tag.ABCData.methods), 12269)
             self.assertEqual(len(abc_tag.ABCData.metadata), 0)
-            self.assertEqual(len(abc_tag.ABCData.classes), 641)
-            self.assertEqual(len(abc_tag.ABCData.scripts), 641)
-            self.assertEqual(len(abc_tag.ABCData.method_bodies), 12046)
+            self.assertEqual(len(abc_tag.ABCData.classes), 650)
+            self.assertEqual(len(abc_tag.ABCData.scripts), 650)
+            self.assertEqual(len(abc_tag.ABCData.method_bodies), 12220)
