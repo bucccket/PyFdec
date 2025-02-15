634c743d879f0c294fd718b7adc26bdfd180d2f2
Bucccket <37452311+bucccket@users.noreply.github.com>
2025-02-15 01:56:47 +0100

    added Protect

---
+++ pyfdec/tags/Protect.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from typing import ClassVar
+
+from pyfdec.extended_buffer import ExtendedBuffer
+from pyfdec.tags.Tag import Tag
+
+
+@dataclass
+class Protect(Tag):
+    tag_type: ClassVar[Tag.TagTypes] = Tag.TagTypes.Protect
+
+    @classmethod
+    def from_buffer(cls, buffer: ExtendedBuffer) -> 'Protect':
+        return cls()
+
+
+Tag.register(Protect)
